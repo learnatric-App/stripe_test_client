349b57a3daa148d996a76f7980504a9d5c8ddca1
bschaaf1017 <bschaaf1017@gmail.com>
2021-08-03 11:35:28 -0400

    checnged input to subscription

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
-import StripeContainer from "./StripeContainer";
+import React from 'react';
+// Components
+import HomePage from './HomePage';
+// Stripe
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+// Styles
+// import './index.scss';
 
-const App = () => {
+const stripePromise = loadStripe("pk_test_51J2zzME260ZusP4JwfTcUB8mmbeIu09aAv1bixaOocy8VRK6zbagC0kTS94xnJsFRucW8r1ATpq5drjIp1l0vwfr00jUEGf07H");
+
+function App() {
   return (
-    <div className="App">
-      <StripeContainer />
-    </div>
+    <Elements stripe={stripePromise}>
+      <HomePage />
+    </Elements>
   );
-};
+}
 
 export default App;
