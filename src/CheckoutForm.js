af0f0800b9fb363aa8110fed740c5ba589009664
bschaaf1017 <bschaaf1017@gmail.com>
2021-08-04 13:22:56 -0400

    comment checkoutForm

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -1,46 +1,46 @@
-import React from "react";
-import axios from "axios";
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+// import React from "react";
+// import axios from "axios";
+// import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
-export const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
+// export const CheckoutForm = () => {
+//   const stripe = useStripe();
+//   const elements = useElements();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement),
-    });
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     const { error, paymentMethod } = await stripe.createPaymentMethod({
+//       type: "card",
+//       card: elements.getElement(CardElement),
+//     });
 
-    if (!error) {
-      console.log("Stripe 23 | token generated!", paymentMethod);
-      try {
-        const { id } = paymentMethod;
-        const response = await axios.post(
-          "http://localhost:8081/stripe/charge",
-          {
-            amount: 999,
-            id: id,
-          }
-        );
+//     if (!error) {
+//       console.log("Stripe 23 | token generated!", paymentMethod);
+//       try {
+//         const { id } = paymentMethod;
+//         const response = await axios.post(
+//           "http://localhost:8081/stripe/charge",
+//           {
+//             amount: 999,
+//             id: id,
+//           }
+//         );
 
-        console.log("Stripe 35 | data", response.data.success);
-        if (response.data.success) {
-          console.log("CheckoutForm.js 25 | payment successful!");
-        }
-      } catch (error) {
-        console.log("CheckoutForm.js 28 | ", error);
-      }
-    } else {
-      console.log(error.message);
-    }
-  };
+//         console.log("Stripe 35 | data", response.data.success);
+//         if (response.data.success) {
+//           console.log("CheckoutForm.js 25 | payment successful!");
+//         }
+//       } catch (error) {
+//         console.log("CheckoutForm.js 28 | ", error);
+//       }
+//     } else {
+//       console.log(error.message);
+//     }
+//   };
 
-  return (
-    <form onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
-      <CardElement />
-      <button>Pay</button>
-    </form>
-  );
-};
+//   return (
+//     <form onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
+//       <CardElement />
+//       <button>Pay</button>
+//     </form>
+//   );
+// };
